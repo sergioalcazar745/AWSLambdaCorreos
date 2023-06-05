ffcdf77cbc4b61e1b303e8f93c88a99e89ea4c1e
sergioalcazar745 <sergioalcazar745@gmail.com>
2023-06-05 14:24:37 +0200

    casi casi

--- AWSLambdaCorreos/Function.cs
+++ AWSLambdaCorreos/Function.cs
@@ -7,7 +7,7 @@ using MVCApiExtraSlice.Helpers;
 using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+//[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace AWSLambdaCorreos;
 
